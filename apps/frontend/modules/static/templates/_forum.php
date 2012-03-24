90d8f658de7ed76f1f54ba6aa7d38081604db051
Fan Deng <leondeng@gmail.com>
2012-03-24 23:23:14 +1100

    tune navgations

---
+++ apps/frontend/modules/static/templates/_forum.php
@@ -0,0 +1,8 @@
+<div id="ContentLeft">
+	<div class="">
+
+		<iframe src="http://www.hapkido.com.au/forum/" frameborder="0"
+			height="900px" width="870px" scrolling="no"></iframe>
+	</div>
+</div>
+<?php include_partial('Koreanword');?>
