9d8dfd8a37bc7b9da4d24429853dd2ef2786f5f0
Fan Deng <leondeng@gmail.com>
2012-03-24 22:03:45 +1100

    recover normal

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -1,15 +1,14 @@
-<?php $sub = sfConfig::get('app_deploy_path'); ?>
 <div id="Footer" class="tl">
 	<div class="content">
 		<ul>
-			<li><a href="<?php echo $sub;?>/about_iha.html">ABOUT INTERNATIONAL HAPKIDO
+			<li><a href="<?php echo url_for('@static?content=about_iha'); ?>">ABOUT INTERNATIONAL HAPKIDO
 					ALLIANCE</a></li>
 			<li><span>|</span></li>
-			<li><a href="<?php echo $sub;?>/privacy_policy.html">PRIVACY POLICY</a></li>
+			<li><a href="<?php echo url_for('@static?content=privacy_policy'); ?>">PRIVACY POLICY</a></li>
 			<li><span>|</span></li>
-			<li><a href="<?php echo $sub;?>/term_of_use.html">TERM OF USE</a></li>
+			<li><a href="<?php echo url_for('@static?content=term_of_use'); ?>">TERM OF USE</a></li>
 			<li><span>|</span></li>
-			<li><a href="<?php echo $sub;?>/contact_us.html">CONTACT US</a></li>
+			<li><a href="<?php echo url_for('@static?content=contact_us'); ?>">CONTACT US</a></li>
 			<li><span>|</span></li>
 			<li><span>&copy; 2012</span></li>
 		</ul>
