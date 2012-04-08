3cce1f1b859dfb1e315d6d9963d2eea0ccdee4b8
Fan Deng <leondeng@gmail.com>
2012-04-08 12:54:21 +1000

    userAdmin, edit school style done

--- apps/frontend/modules/userAdmin/templates/editSchoolSuccess.php
+++ apps/frontend/modules/userAdmin/templates/editSchoolSuccess.php
@@ -1,8 +1,8 @@
 <form action="<?php echo url_for('@edit_school') ?>" method="post">
 	<?php echo $form->renderHiddenFields() . $form->renderGlobalErrors(); ?>
-	<div class="box edit_dojang">
-		<div class="title_login">Welcome Dojang,</div>
-		<div class="inner_dojang">
+	<div class="box edit">
+		<div class="title">Welcome Dojang,</div>
+		<div class="inner">
 			<div class="column">
 				<?php echo $form['name']->renderRow(); ?>
 				<?php echo $form['country']->renderRow(); ?>
@@ -19,15 +19,22 @@
 				<?php echo $form['class_time']->renderRow(); ?>
 			</div>
 		</div>
+		<div class="clear"></div>
 		<div class="button_cancel">
 			<a href="<?php echo url_for('@userAdmin'); ?>"><input type="button"
 				name="Submit" value="Cancel" class="button" /> </a>
 		</div>
-		<div class="button_login">
+		<div class="button_edit">
 			<input class="button" value="Submit" type="submit" />
 		</div>
+		<div class="terms">
+			By submitting you agree to our <a href="<?php echo url_for('@static?content=term_of_use'); ?>">Terms of Use</a>.<br>The Headquarter will
+			evaluate your details before they appear online.
+		</div>
 	</div>
-	<div class="img_right">
-		<?php echo content_tag('img', '', array('src' => 'images/blackbelt.png', 'width' => 300, 'height' => 576)); ?>
+	<div class="img_edit">
+		<?php echo content_tag('img', '', array('src' => 'images/blackbelt.png', 'width' => 273, 'height' => 524)); ?>
 	</div>
+	
 </form>
+<div class="clear"></div>
