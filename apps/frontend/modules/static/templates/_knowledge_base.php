4c37a4e7d013dac1736ad8488235a568fd396137
Fan Deng <leondeng@gmail.com>
2012-04-11 00:07:17 +1000

    knowledge base home page done

---
+++ apps/frontend/modules/static/templates/_knowledge_base.php
@@ -0,0 +1,44 @@
+<div id="ContentLeft">
+	<div id="KnowledgeGrids" class="content_all">
+		<h1 class="page_title">Knowledge Base</h1>
+		<div class="grid icon">
+			<img src="images/Dobok_Folding.png" />
+		</div>
+		<div class="grid text">
+			<div class="title">Dobok Folding</div>
+			<div class="summary">We show you in detail the best way to fold you
+				Dobok.</div>
+		</div>
+
+		<div class="grid icon">
+			<img src="images/Korean_Terminology.png" />
+		</div>
+		<div class="grid text widen">
+			<div class="title">Korean Terminology</div>
+			<div class="summary">A basic guide to Korean words you should know in
+				the exciting world of Hapkido.</div>
+		</div>
+
+		<div class="clear"></div>
+		<div class="grid icon">
+			<img src="images/Belt_Tying.png" />
+		</div>
+		<div class="grid text">
+			<div class="title">Belt Tying</div>
+			<div class="summary">The Preferred IHA Method to tye your belt and
+				keep it on.</div>
+		</div>
+
+		<div class="grid icon">
+			<img src="images/The_Path_to_Black_Belt.png" />
+		</div>
+		<div class="grid text widen">
+			<div class="title">The Path to Black Belt</div>
+			<div class="summary">A little guide for the journey to Black Belt,
+				but nothing can ever prepare you for this.</div>
+		</div>
+
+	</div>
+</div>
+<?php include_partial('KoreanWord'); ?>
+<?php use_stylesheet('knowledge_base'); ?>
