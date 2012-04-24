99444043f780db2f379c51d3cc01b3651af434a3
Fan Deng <leondeng@gmail.com>
2012-04-24 21:50:46 +1000

    story13: terminology page done

--- apps/frontend/modules/static/templates/_KoreanTerminology.php
+++ apps/frontend/modules/static/templates/_KoreanTerminology.php
@@ -1,28 +1,18 @@
-<?php $sections = array(
-    array('label' => 'Basic Terms', 'terms' => array(
-        'Breathing' => 'Tan Jun',
-        'School' => 'Dojang',
-        'Attention' => 'Cha Ryuht',
-        'Bow' => 'Kyung Neh',
-        'Ready Stance' => 'Jhoon Bee',
-        'Uniform' => 'Dobok',
-        'Start' => 'Shi Jak',
-        'Stop' => 'Koman',
-        'Short Stick' => 'Dan Bong',
-        'Spirit Yell' => 'Kiyap',
-    )),
+<?php
+$path = sfConfig::get( 'sf_root_dir' ) . '/apps/frontend/modules/static/config/app.yml';
+include ( sfContext::getInstance()->getConfigCache()->checkConfig($path));
 
-
-); ?>
+$sections = sfConfig::get('app_KoreanTerms_sections');
+?>
 <div id="ContentLeft" class="alignleft">
 	<div class="page_title">Korean Terminology</div>
 	<?php foreach($sections as $section) :?>
-	<div class="term_section">
+	<div class="term_section  column<?php echo $section['column']; ?>">
 		<div class="section_title">
 			<?php echo $section['label']; ?>
 		</div>
-		<?php foreach($section['terms'] as $key => $term) : ?>
 		<div class="section_body">
+			<?php foreach($section['terms'] as $key => $term) : ?>
 			<div class="column">
 				<dl>
 					<dt>
@@ -33,12 +23,10 @@
 					</dd>
 				</dl>
 			</div>
+			<?php endforeach; ?>
 		</div>
-
-		<?php endforeach; ?>
-
-		<?php endforeach; ?>
-
+		<div class="clear"></div>
 	</div>
+	<?php endforeach; ?>
 </div>
 <?php include_partial('KoreanWord'); ?>
